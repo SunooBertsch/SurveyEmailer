38d3e944f83bbb5934d1880156377de715f1b24f
SunooBertsch <sbertsch92@yahoo.com>
2017-08-21 16:40:06 -0700

    fixed package.json

--- server/index.js
+++ server/index.js
@@ -4,7 +4,5 @@ const app = express();
 app.get("/", (req, res) => {
   res.send({ hi: "there" });
 });
-app.get("/bye", (req, res) => {
-  res.send({ hello: "hello" });
-});
-app.listen(5000);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
