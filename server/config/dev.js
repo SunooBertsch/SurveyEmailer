b26c3a957c8026df28fbb004455d1d9a9f45c147
SunooBertsch <sbertsch92@yahoo.com>
2017-09-15 13:17:47 -0700

    implemented production keys via heroku env variables

---
+++ server/config/dev.js
@@ -0,0 +1,7 @@
+module.exports = {
+  googleClientID:
+    "749299928695-0cjl10n226qtod3385sctnh4digvdfj4.apps.googleusercontent.com",
+  googleClientSecret: "9v_wy8Pc0_cg-5pUVlYr_QSg",
+  mongoURI: "mongodb://sunoo:Forest92@ds133084.mlab.com:33084/server_mailer",
+  cookieKey: "jkabchebkahwajdnfdnwhequiaehjdshkjxz"
+};
