34e3b39ff17d3c9679e22060a3cab8c9fe494bf7
SunooBertsch <sbertsch92@yahoo.com>
2017-09-21 13:16:01 -0700

    init

--- server/config/dev.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  googleClientID:
-    "749299928695-0cjl10n226qtod3385sctnh4digvdfj4.apps.googleusercontent.com",
-  googleClientSecret: "9v_wy8Pc0_cg-5pUVlYr_QSg",
-  mongoURI:
-    "mongodb://devsunoo:28951Rockport@ds133084.mlab.com:33084/server_mailer",
-  cookieKey: "dwuoahdknxjkhgwiuadhwahjoi"
-};
