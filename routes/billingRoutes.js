74d4f70f53e299e4d25a8c92dbb09b5c3455a637
SunooBertsch <sbertsch92@yahoo.com>
2017-09-27 14:06:55 -0700

    server side user auth middleware created

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -1,13 +1,21 @@
 const keys = require("../config/keys");
 const stripe = require("stripe")(keys.stripeSecretKey);
+const requireLogin = require("../middlewares/requireLogin");
 
 module.exports = app => {
-  app.post("/api/stripe", (req, res) => {
-    stripe.charges.create({
+  app.post("/api/stripe", requireLogin, async (req, res) => {
+    if (!req.user) {
+      return res.status(401).send({ error: "You must log into your account!" });
+    }
+    const charge = await stripe.charges.create({
       amount: 500,
-      current: "usd",
+      currency: "usd",
       description: "$5 for 5 credits",
       source: req.body.id
     });
+
+    req.user.credits += 5;
+    const user = await req.user.save();
+    res.send(user);
   });
 };
